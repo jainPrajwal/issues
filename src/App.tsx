4b747522067e04d1222bad8fe27a4ee1e36d6b46
unknown <jainprajwal123@gmail.com>
2022-09-15 19:19:10 +0530

    feat: types added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
